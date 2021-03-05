ce1396df32fcef753893ec8e288500a8eae078a5
chrisPicano <cpicano25@gmail.com>
2021-03-05 12:30:06 -0500

    Adding classes and files

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+console.log("Team Profile Generator")
+const Employee = require("./Employee");
+const Manager = require("./Manager");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
